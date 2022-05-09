72334f9b9dd2fcce8238c141103175368c206a3f
chupacubra <105209369+chupacubra@users.noreply.github.com>
2022-05-09 15:39:33 +0300

    garbage

--- lua/entities/bucket/init.lua
+++ lua/entities/bucket/init.lua
@@ -2,40 +2,17 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
  
 include('shared.lua')
---[[
-function ENT:SetupDataTables()
-	self:NetworkVar( "String", 1,"Content" )
-end
---]]
+
 function ENT:Initialize()
 	self:SetModel( "models/props_junk/MetalBucket01a.mdl" )
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_VPHYSICS )
 	self:SetSolid( SOLID_VPHYSICS )
-  self:SetUseType(SIMPLE_USE)
-  local phys = self:GetPhysicsObject()
+	self:SetUseType(SIMPLE_USE)
+	local phys = self:GetPhysicsObject()
 	if (phys:IsValid()) then
 		phys:Wake()
 	end
-  self.content = {}
+	self.content = {}
+	self.limit = 120
 end
---[[
-function ENT:Use( activator, caller )
-  --PrintTable(self.content)
-  CHEMIC:AddComp("water",50,self)
-  --CHEMIC:AddComp("potassi",50,self)
-  CHEMIC:AddComp("tealeaf",5,self)
-  CHEMIC:MixComp(self)
-  PrintTable(self.content)
-end
-
-
-function ENT:Think()
-  local tbl = FormContent(self)
-  local str = ""
-  for k,v in pairs(tbl) do
-    str= str..k .."="..v.."/"
-  end
-  self:SetContent(str)
-end
--]]
